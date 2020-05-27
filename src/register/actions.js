1569407c6e4d62eb91799b50a82e0c701003286a
OnyJ <julien.josselin75@hotmail.fr>
2020-05-27 18:18:04 +0200

    move fetchRegister from store.js to register/actions.js

--- src/register/actions.js
+++ src/register/actions.js
@@ -1,6 +1,11 @@
 const FETCH_REGISTER_REQUEST = "FETCH_REGISTER_REQUEST";
 const FETCH_REGISTER_SUCCESS = "FETCH_REGISTER_SUCCESS";
 const FETCH_REGISTER_FAILURE = "FETCH_REGISTER_FAILURE";
+const registerData = {
+  username: "eh6",
+  email: "eh6@heheh.com",
+  password: "hehehehe6",
+};
 
 export const fetchRegisterRequest = () => {
   return {
@@ -19,5 +24,29 @@ export const fetchRegisterFailure = (error) => {
   return {
     type: FETCH_REGISTER_FAILURE,
     error,
+  };
+};
+
+export const fetchRegister = () => {
+  return (dispatch) => {
+    dispatch(fetchRegisterRequest());
+    fetch("https://api-minireseausocial.mathis-dyk.fr/auth/local/register", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(registerData),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        if (response.error) {
+          dispatch(
+            fetchRegisterFailure(response.message[0].messages[0].message)
+          );
+        } else {
+          dispatch(fetchRegisterSuccess(response.user));
+        }
+      });
   };
 };
--- src/store.js
+++ src/store.js
@@ -1,11 +1,8 @@
 import { createStore, compose, applyMiddleware } from "redux";
-import registerReducer from "./register/reducer";
 import thunkMiddleware from "redux-thunk";
-import {
-  fetchRegisterRequest,
-  fetchRegisterSuccess,
-  fetchRegisterFailure,
-} from "./register/actions";
+
+import registerReducer from "./register/reducer";
+import { fetchRegister } from "./register/actions";
 
 const store = createStore(
   registerReducer,
@@ -14,36 +11,6 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
-
-const registerData = {
-  username: "eh6",
-  email: "eh6@heheh.com",
-  password: "hehehehe6",
-};
-
-const fetchRegister = () => {
-  return (dispatch) => {
-    dispatch(fetchRegisterRequest());
-    fetch("https://api-minireseausocial.mathis-dyk.fr/auth/local/register", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(registerData),
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        if (response.error) {
-          dispatch(
-            fetchRegisterFailure(response.message[0].messages[0].message)
-          );
-        } else {
-          dispatch(fetchRegisterSuccess(response.user));
-        }
-      });
-  };
-};
 
 store.subscribe(() => {
   console.log("Store is now ", store.getState());
