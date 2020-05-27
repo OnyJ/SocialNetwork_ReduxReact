256c56abdb7af5665ada602dd8f7c069b3fdf4a2
OnyJ <julien.josselin75@hotmail.fr>
2020-05-27 20:24:27 +0200

    new pages included in src/index.js and Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import LogOut from "../connexion/LogOut";
+
+const loggedOutNavbar = (
+  <nav>
+    <Link to="/">Home</Link>
+    <br />
+    <Link to="/login">Log In</Link>
+    <br />
+    <Link to="/register">Sign Up</Link>
+    <br />
+  </nav>
+);
+
+const loggedInNavbar = (
+  <nav>
+    <Link to="/">Home</Link>
+    <br />
+    <Link to="/profile">Profile</Link>
+    <br />
+    <a href="#" onClick={LogOut}>
+      Log Out
+    </a>
+    <br />
+  </nav>
+);
+
+const Navbar = () => {
+  return (
+    <>{true ? <div>{loggedInNavbar}</div> : <div>{loggedOutNavbar}</div>}</>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+// Router
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./home/Home";
+import LogIn from "./connexion/LogIn";
+import Profile from "./profile/Profile";
+import OtherProfile from "./profile/OtherProfile";
+// Components
+import Register from "./register/Register";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
-// Components
-import Register from "./register/Register";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Register />
-  </Provider>,
+  <>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/user/:user_name">
+            <OtherProfile />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
+  </>,
   document.getElementById("root")
 );
 
