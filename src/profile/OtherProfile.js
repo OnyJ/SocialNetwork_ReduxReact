ca5a7667fee6b795c2cba81aae9e372ee4de554a
OnyJ <julien.josselin75@hotmail.fr>
2020-05-27 20:16:20 +0200

    create profile pages (MyProfile, OtherProfile)

---
+++ src/profile/OtherProfile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const OtherProfile = () => (
+  <div>
+    <h1>Author's Profile Name</h1>
+    <p>Description</p>
+    <br />
+    <p>Post 1</p>
+    <p>Post 2</p>
+    <p>Post 3</p>
+    <p>Post 4</p>
+  </div>
+);
+
+export default OtherProfile;
