880377a8dbdb6868969e7593b7da4850c27dc5a1
OnyJ <julien.josselin75@hotmail.fr>
2020-05-27 20:15:54 +0200

    create connexion pages (login logout)

---
+++ src/connexion/LogOut.js
@@ -0,0 +1,9 @@
+// import React from "react";
+
+const LogOut = () => {
+  // faire un useState ou utiliser redux mais ne pas changer connected en dur
+  // ça va se passer dans les cookies
+  // ça devrait recharger la page en mode déconnecté automatiquement
+};
+
+export default LogOut;
