709cb820b8012228de19f3d425ad392b0424ed82
OnyJ <julien.josselin75@hotmail.fr>
2020-05-28 11:18:24 +0200

    write instructions in LogIn file

--- src/connexion/LogIn.js
+++ src/connexion/LogIn.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+// redux, si on obtient un reducer success, alors ça va redirect vers home
+// connexion auto si c'est dans les cookies (cookies.get...)
 const LogIn = () => <h1>Log in</h1>;
 
 export default LogIn;
